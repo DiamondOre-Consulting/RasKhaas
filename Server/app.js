f4239d3a44303a1db32b2b0ef1915f6a97655189
Zoya774 <zoya.rasonline@gmail.com>
2025-02-03 10:36:21 +0530

    update url

--- Server/app.js
+++ Server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173' , "https://raskhaas-frontend.onrender.com"],
     credentials : true
 }))
 
